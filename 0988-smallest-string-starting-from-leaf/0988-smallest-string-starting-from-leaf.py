6ab4042d4a325071496c641e29af9db65c4f9a03
dvm <146653758+Divyam895@users.noreply.github.com>
2026-08-18 19:26:27 +0530

    Time: 8 ms (22.8%), Space: 20.3 MB (80.66%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        mini=None
+        print("hello")
+        def trav(par,node):
+            nonlocal mini
+            if not node:
+                return
+            if not node.right and not node.left:
+                par=chr(97+node.val)+par
+                print(par)
+                if mini is None or par<mini:
+                    mini=par
+                return
+            trav(chr(97+node.val)+par,node.left)
+            trav(chr(97+node.val)+par,node.right)
+        trav("",root)
+        return mini
